422d96e8520552b1f56801b06f1d9b559812d4fc
mmalenic <mmalenic1@gmail.com>
2026-04-09 13:27:47 +1000

    feat: set jsonata invoke args for bucket definitions

--- packages/steps-s3-copy/src/steps-s3-copy-construct.ts
+++ packages/steps-s3-copy/src/steps-s3-copy-construct.ts
@@ -188,6 +188,9 @@ export class StepsS3CopyConstruct extends Construct {
 
       // if not passed, default to ""
       [RETAIN_COPY_REPORT_FIELD_NAME]: `{% [ $states.input.${RETAIN_COPY_REPORT_FIELD_NAME}, "" ][0] %}`,
+
+      // bucket definitions default to empty object when not provided
+      bucketDefinitions: `{% $exists($states.input.bucketDefinitions) ? $states.input.bucketDefinitions : {} %}`,
     };
     const jsonataInvokeSettings: {
       [K in keyof StepsS3CopyInvokeSettings]: string;
