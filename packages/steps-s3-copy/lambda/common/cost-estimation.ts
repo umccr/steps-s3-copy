34fbb04e7b42a147c4ddc8fedeba38d0e02bf0a8
Felipe Jimenez-Ibarra <felipejimenezibarra@gmail.com>
2026-06-09 10:32:07 +1000

    Completed the regionPrefixMap list and added a fallback for unknown regions.

--- packages/steps-s3-copy/lambda/common/cost-estimation.ts
+++ packages/steps-s3-copy/lambda/common/cost-estimation.ts
@@ -440,20 +440,41 @@ export type ComputeCosts = {
   };
 };
 
+// AWS Region billing codes: https://docs.aws.amazon.com/global-infrastructure/latest/regions/aws-region-billing-codes.html
 const regionPrefixMap: Record<string, string> = {
   "us-east-1": "USE1",
   "us-east-2": "USE2",
   "us-west-1": "USW1",
   "us-west-2": "USW2",
-  "ap-southeast-1": "APS1",
-  "ap-southeast-2": "APS2",
+  "ca-central-1": "CAN1",
+  "ca-west-1": "CAN2",
+  "mx-central-1": "MXC1",
+  "af-south-1": "AFS1",
+  "ap-east-1": "APE1",
+  "ap-east-2": "APE2",
   "ap-northeast-1": "APN1",
   "ap-northeast-2": "APN2",
+  "ap-northeast-3": "APN3",
+  "ap-southeast-1": "APS1",
+  "ap-southeast-2": "APS2",
   "ap-south-1": "APS3",
+  "ap-southeast-3": "APS4",
+  "ap-south-2": "APS5",
+  "ap-southeast-4": "APS6",
+  "ap-southeast-5": "APS7",
+  "ap-southeast-6": "APS8",
+  "ap-southeast-7": "APS9",
   "eu-west-1": "EU",
-  "eu-west-2": "EUW2",
   "eu-central-1": "EUC1",
-  "ca-central-1": "CAN1",
+  "eu-central-2": "EUC2",
+  "eu-west-2": "EUW2",
+  "eu-west-3": "EUW3",
+  "eu-north-1": "EUN1",
+  "eu-south-1": "EUS1",
+  "eu-south-2": "EUS2",
+  "il-central-1": "ILC1",
+  "me-central-1": "MEC1",
+  "me-south-1": "MES1",
   "sa-east-1": "SAE1",
 };
 
@@ -522,6 +543,10 @@ async function fetchFargateComputePrice(
   const client = new PricingClient({ region: "us-east-1" });
 
   const regionPrefix = regionPrefixMap[region];
+
+  if (!regionPrefix) {
+    throw new Error(`Unsupported AWS region for pricing lookup: ${region}`);
+  }
 
   const [vcpuResponse, memResponse] = await Promise.all([
     client.send(
